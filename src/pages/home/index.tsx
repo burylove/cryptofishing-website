59880a4d77e766658a223ee3ef025b1755892c31
Davidxing1 <937104001@qq.com>
2021-11-16 23:38:28 +0800

    test 更新组件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -408,7 +408,7 @@ export default function Home() {
 
                                    </button>
                                    <a
-                                       href="https://unity3d-test.vercel.app/"
+                                       href="https://unity3d-beta.vercel.app/"
                                        className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-xl font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                                    >
                                        PLAY
